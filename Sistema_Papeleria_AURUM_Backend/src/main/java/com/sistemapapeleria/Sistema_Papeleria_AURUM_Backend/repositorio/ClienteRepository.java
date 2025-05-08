354d014b21310d946bf0f21e6747c4a9961197a2
Semiramyz <js0207367@gmail.com>
2025-05-08 17:24:21 -0500

    v.0.0.15

--- Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/repositorio/ClienteRepository.java
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/repositorio/ClienteRepository.java
@@ -3,7 +3,7 @@ package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.entidades.Cliente;
+import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Entidades.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
