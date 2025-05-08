880f0d558f0ace59ad1b0c4a07055f0d860cf99f
Semiramyz <js0207367@gmail.com>
2025-05-08 00:46:23 -0500

    V.3

---
+++ Aurum_Front/PFA/src/app/pages/impuesto/impuesto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-impuesto',
+  imports: [],
+  templateUrl: './impuesto.component.html',
+  styleUrl: './impuesto.component.scss'
+})
+export class ImpuestoComponent {
+
+}
