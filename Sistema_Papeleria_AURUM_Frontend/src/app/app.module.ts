64c007c51229c5f40491990db7499e456cd5fe87
Semiramyz <js0207367@gmail.com>
2025-05-08 14:34:58 -0500

    v.0.0.7

--- Sistema_Papeleria_AURUM_Frontend/src/app/app.module.ts
+++ Sistema_Papeleria_AURUM_Frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClsComponent } from './pestañas/cls/cls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClsComponent
   ],
   imports: [
     BrowserModule,
