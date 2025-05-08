13649405cbe4c6e2bd1e3035a1202d6fca772b61
Semiramyz <js0207367@gmail.com>
2025-05-08 13:13:03 -0500

    v.0.0.1

---
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/Entidades/Cliente.java
@@ -0,0 +1,9 @@
+package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Entidades;
+
+public class Cliente {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String codigo;
+
+}
---
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/SistemaPapeleriaAurumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPapeleriaAurumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPapeleriaAurumBackendApplication.class, args);
+	}
+
+}
