8b6cce8045633e7b3b0ecf8c4f789758f8ec93f6
Semiramyz <js0207367@gmail.com>
2025-05-08 13:21:55 -0500

    v.0.0.2

--- Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/Entidades/Cliente.java
+++
@@ -1,9 +0,0 @@
-package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Entidades;
-
-public class Cliente {
-    private String id;
-    private String nombre;
-    private String apellido;
-    private String codigo;
-
-}
