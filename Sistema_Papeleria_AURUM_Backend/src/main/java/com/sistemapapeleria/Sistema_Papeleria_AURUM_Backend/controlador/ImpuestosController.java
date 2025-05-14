f02ba8f93f04a40a1b999713dc840cb8b38ad9d5
Giaxeri <giaxerigd@gmail.com>
2025-05-13 19:29:03 -0500

    v.0.0.33

--- Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/controlador/ImpuestosController.java
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/controlador/ImpuestosController.java
@@ -1,9 +1,17 @@
 package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.controlador;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Modelo.ImpuestosDTO;
 import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Servicio.ImpuestosService;
