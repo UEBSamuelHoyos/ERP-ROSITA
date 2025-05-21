af5e316fffbc877f37a8ad84b6e7667b660725d7
Kuo <146367592+UEBSamuelHoyos@users.noreply.github.com>
2025-05-21 09:21:15 -0500

    fg
    dfsdfsd

--- Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/Servicio/CuentasPagarService.java
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/Servicio/CuentasPagarService.java
@@ -1,5 +1,6 @@
 package com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Servicio;
 
+<<<<<<< Updated upstream
 import java.util.List;
 import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Modelo.CuentasPagarDTO;
 
@@ -14,5 +15,33 @@ public interface CuentasPagarService {
     CuentasPagarDTO updateCuentaPagar(Long id, CuentasPagarDTO dto);
     List<CuentasPagarDTO> getCuentasByProveedorId(Long proveedorId);
     CuentasPagarDTO marcarComoPagada(Long id);
+}
+>>>>>>> Stashed changes
+=======
+import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Entidades.CuentasPagar;
+import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.repositorio.CuentasPagarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CuentasPagarService {
+
+    @Autowired
+    private CuentasPagarRepository repository;
+
+    public CuentasPagar guardarCuenta(CuentasPagar cuenta) {
+        return repository.save(cuenta);
+    }
+
+    public Optional<CuentasPagar> editarCuenta(Long id, CuentasPagar datosActualizados) {
+        return repository.findById(id).map(cuenta -> {
+            cuenta.setProveedorId(datosActualizados.getProveedorId());
+            cuenta.setMonto(datosActualizados.getMonto());
+            cuenta.setEstado(datosActualizados.getEstado());
+            return repository.save(cuenta);
+        });
+    }
 }
 >>>>>>> Stashed changes
--- Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/repositorio/CuentasPagarRepository.java
+++ Sistema_Papeleria_AURUM_Backend/src/main/java/com/sistemapapeleria/Sistema_Papeleria_AURUM_Backend/repositorio/CuentasPagarRepository.java
@@ -6,17 +6,23 @@ import java.util.List;
 
 >>>>>>> Stashed changes
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< Updated upstream
 import org.springframework.stereotype.Repository;
 
 import com.sistemapapeleria.Sistema_Papeleria_AURUM_Backend.Entidades.CuentasPagar;
+=======
+>>>>>>> Stashed changes
 
-@Repository
 public interface CuentasPagarRepository extends JpaRepository<CuentasPagar, Long> {
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 }
 =======
 
     // Buscar todas las cuentas asociadas a un proveedor
     List<CuentasPagar> findByProveedorId(Long proveedorId);
+}
+>>>>>>> Stashed changes
+=======
 }
 >>>>>>> Stashed changes
